fefdd46136aca67d63b27821ce0b02a8d9ad5a07
Chiara <mc.nicoletti@hotmail.it>
2019-02-19 16:35:19 +0100

    il mio primo commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
@@ -8,15 +9,28 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  myForm: FormGroup;
-   constructor() {
-     this.myForm = new FormGroup({
-       Nome: new FormControl(),
-       Cognome: new FormControl(),
-       citta: new FormControl(),
-       data: new FormControl()
+  mioForm: FormGroup;
+   ngOnInit(): void {
+     this.mioForm = new FormGroup({
+       'name': new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)]),
+       'cognome': new FormControl('',
+       [Validators.required,
+          Validators.minLength(2)]),
+       'citta': new FormControl('',
+       [Validators.required,
+          Validators.minLength(2)]),
+        'email':new FormControl('',
+        [Validators.required, Validators.email]),
      });
    }
-  ngOnInit() {
-   }
+   get name() { return this.mioForm.get('name'); }
+   get cognome() { return this.mioForm.get('cognome'); }
+   get citta() { return this.mioForm.get('citta'); }
+   get email() { return this.mioForm.get('email'); }
+
+   print() {
+     console.log(this.mioForm.value); }
+
  }
